6cc51dee6c9606a50775eb3d13f98ff0254dcab2
Ankur Pathak <ankurpathak@live.in>
2019-04-11 14:10:59 +0530

    Another Job

--- src/main/java/com/github/ankurpathak/quartz/Main.java
+++ src/main/java/com/github/ankurpathak/quartz/Main.java
@@ -1,4 +1,38 @@
 package com.github.ankurpathak.quartz;
 
+import com.github.ankurpathak.quartz.job.HelloJob;
+import org.quartz.*;
+import org.quartz.impl.StdSchedulerFactory;
+
+import java.util.concurrent.TimeUnit;
+
 public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        try {
+            // Grab the Scheduler instance from the Factory
+            Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+
+            // and start it off
+            scheduler.start();
+
+            JobDetail job = JobBuilder.newJob(HelloJob.class)
+                    .withIdentity("job1", "group1")
+                    .build();
+            Trigger trigger = TriggerBuilder.newTrigger()
+                    .withIdentity("trigger1", "group1")
+                    .startNow()
+                    .withSchedule(SimpleScheduleBuilder.simpleSchedule().withIntervalInSeconds(10).repeatForever())
+                    .build();
+
+            scheduler.scheduleJob(job, trigger);
+
+
+            TimeUnit.MINUTES.sleep(12);
+
+            scheduler.shutdown();
+
+        } catch (SchedulerException se) {
+            se.printStackTrace();
+        }
+    }
 }
--- src/main/java/com/github/ankurpathak/quartz/job/HelloJob.java
+++ src/main/java/com/github/ankurpathak/quartz/job/HelloJob.java
@@ -1,4 +1,14 @@
 package com.github.ankurpathak.quartz.job;
 
-public class HelloJob {
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+import javax.tools.JavaCompiler;
+
+public class HelloJob implements Job {
+    @Override
+    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException{
+        System.out.println("Hello, Job!!!");
+    }
 }
